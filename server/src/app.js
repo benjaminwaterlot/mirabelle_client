d3bf41deb29173a32189bfefec4ccc279ac2aace
Benjamin <waterlotbenjamin@gmail.com>
2019-02-20 09:57:18 +0100

    advancing server side

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,76 @@
-console.log("Coucou toi")
+const express = require('express');
+// import express from 'express'
+
+const { ApolloServer, gql } = require('apollo-server-express');
+
+let customers = [
+    {
+        id: 1,
+        name: "Benjamin",
+    },
+    {
+        id: 2,
+        name: "Rodolphe",
+    },
+    {
+        id: 3,
+        name: "Antoine",
+    },
+]
+
+const typeDefs = gql`
+    type Client {
+        id: Int
+        name: String
+    }
+    type Query {
+        clients: [Client]
+        client(id: Int, name: String): Client
+    }
+    type Mutation {
+        addCustomer(id: Int, name: String): Client
+        deleteCustomer(id: Int, name: String): Client
+    }
+`;
+
+const resolvers = {
+    Query: {
+      clients: () => customers,
+      client: (parent, { id, name }) => {
+          console.log(parent);
+          if (id) return customers[id];
+          if (name) return customers.find((customer) => customer.name == name);
+        }
+    },
+    Mutation: {
+        addCustomer: (parent, { name }) => {
+            const last_id = customers[customers.length - 1].id
+            customers = [...customers, { id: last_id + 1, name: name}];
+            return customers[customers.length - 1];
+        },
+        deleteCustomer: (parent, { id, name }) => {
+            let remove
+            console.log("parent is " + parent);
+            if (id && name) return Error("CA VA PAS");
+            if (id)
+            {
+                removed = customers.find(customer => customer.id === id);
+                customers = customers.filter(customer => customer.id !== id);
+            }
+            else if (name)
+            {
+                removed = customers.find(customer => customer.name === name);
+                customers = customers.filter(customer => customer.name !== name);
+            }
+            else console.error("WOAOUH");
+            return removed;
+        }
+    }
+  };
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.listen({port: 4000}, () => console.log('Server ready ! 🚀'));
