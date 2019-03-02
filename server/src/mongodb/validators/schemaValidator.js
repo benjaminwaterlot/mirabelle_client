a3eab014295dd9d9fefbf7067cb8dd3f7b0e7a31
Benjamin WATERLOT <waterlotbenjamin@gmail.com>
2019-03-02 15:06:28 +0100

    added jwt libraries to server

--- server/src/mongodb/validators/schemaValidator.js
+++ server/src/mongodb/validators/schemaValidator.js
@@ -1,35 +1,42 @@
 const Ajv = require('ajv');
+
+// SCHEMAS
 import productSchema from '../schemas/products';
 import customerSchema from '../schemas/customers';
 import packSchema from '../schemas/packs';
-// import bundleSchema from '../schemas/bundles';
-// import groupSchema from '../schemas/packs';
+
+const schemas = [
+	{
+		collection: 'products',
+		schema: productSchema,
+	},
+	{
+		collection: 'customers',
+		schema: customerSchema,
+	},
+	{
+		collection: 'packs',
+		schema: packSchema,
+	},
+];
+
+const unknownSchemaError = schema => ({
+	isValid: false,
+	message: `ERROR : Unknown schema type : >${schema.collection}<`,
+});
+
+const validationObject = validation => ({
+	isValid: validation,
+	message: ajv.errorsText(validation.errors) + '\n',
+});
 
 export default (documentToValidate, type) => {
 	const ajv = new Ajv({ allErrors: true });
-	let schema;
-	switch (type) {
-		case 'product':
-			schema = productSchema;
-			break;
-		case 'customer':
-			schema = customerSchema;
-			break;
-		case 'bundle':
-			schema = bundleSchema;
-			break;
-		case 'pack':
-			schema = packSchema;
-			break;
-		case 'group':
-			schema = groupSchema;
-			break;
-		default:
-			return {
-				isValid: false,
-				message: `ERROR : Unknown schema type : >${type}<`,
-			};
-	}
-	const validation = ajv.validate(schema, documentToValidate);
-	return { isValid: validation, message: ajv.errorsText(validation.errors) };
+	const schema = schemas.find(schema => schema.type === type);
+
+	if (!schema) return unknownSchemaError;
+
+	const validation = ajv.validate(schema.schema, documentToValidate);
+
+	return validation;
 };
--- server/src/tests/testSchemas.js
+++ server/src/tests/testSchemas.js
@@ -3,24 +3,24 @@ import schemaValidator from '../mongodb/validators/schemaValidator';
 import sampleCustomer from './sampleCustomer';
 import sampleProduct from './sampleProduct';
 
-export default async () => {
-	const tests = [
-		{
-			type: 'product',
-			sample: sampleProduct,
-		},
-		{
-			type: 'customer',
-			sample: sampleCustomer,
-		},
-	];
+const tests = [
+	{
+		collection: 'products',
+		sample: sampleProduct,
+	},
+	{
+		collection: 'customers',
+		sample: sampleCustomer,
+	},
+];
 
+export default async () => {
 	for (const test of tests) {
-		const testResult = await schemaValidator(test.sample, test.type);
+		const testResult = await schemaValidator(test.sample, test.collection);
 		if (testResult.isValid)
-			console.log('✓ Schema test in [bundles]: Success');
+			console.log(`✓ Schema test in [${test.collection}]: Success`);
 		else {
-			console.error('✗ Schema test in [bundles]: Error');
+			console.error(`✗ Schema test in [${test.collection}]: Error`);
 			console.error(testResult.message);
 		}
 	}
