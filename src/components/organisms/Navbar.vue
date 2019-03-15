52bf50900aeab611a8e9f9b2b7a096a0bc0b7419
Benjamin <waterlotbenjamin@gmail.com>
2019-03-15 14:05:08 +0100

    minor changes

--- src/components/organisms/Navbar.vue
+++ src/components/organisms/Navbar.vue
@@ -1,10 +1,7 @@
 <template lang='pug'>
 v-toolbar.white.elevation-2(app prominent style="z-index: 1000;")
-	v-btn.ma-0.pa-0(flat to="/" large)
-		v-img.ma-0.clickable.hidden-sm-and-down(src="./logo.png" contain max-height="75%" @click="() => this.$router.push({ path: '/' })")
-	//- v-toolbar-title
-		//- v-btn.ma-0.pa-1(flat to="/" large height="100px")
-		//- 	v-img(src="./logo.png" contain)
+	router-link(to="/")
+		v-img.ma-0.hidden-sm-and-down(src="./logo.png" height="50px" width="140px" contain)
 	v-spacer(grow)
 	v-toolbar-items
 		v-menu(flat v-for="item in items" :key="item.label" bottom offset-y open-on-hover)
@@ -15,102 +12,105 @@ v-toolbar.white.elevation-2(app prominent style="z-index: 1000;")
 	v-btn(v-if="!isAuthenticated" flat color="primary" @click="authenticate()")
 		v-icon(left) account_circle
 		span Me connecter
-	v-btn.ml-4(v-else flat color="primary" to="/mon-espace")
-		v-icon.hidden-md-and-down(left) face
-		v-icon.hidden-lg-and-up face
-		span.hidden-md-and-down Mon espace
+	v-btn.hidden-md-and-down.ml-4(v-if="isAuthenticated" flat color="primary" :to="{name: 'espaceclient'}")
+		v-icon(left) face
+		span Mon espace
+	v-btn.hidden-lg-and-up.ml-4(v-if="isAuthenticated" fab small flat color="primary" :to="{name: 'espaceclient'}")
+		v-icon(medium) face
 </template>
 
 <script>
 import auth from "@/auth/AuthService";
 
 export default {
-	name: "Navbar",
-	props: {
-		isAuthenticated: Boolean,
-		updateStatus: Function
-	},
-	methods: {
-		authenticate: function() {
-			auth.login();
-			this.updateStatus();
-		},
-		logout: function() {
-			auth.logout();
-			this.updateStatus();
-		}
-	},
-	data() {
-		return {
-			items: [
-				{
-					label: "Nos supers paniers",
-					dropdown: [
-						{
-							label: "Le super mix",
-							link: "/"
-						},
-						{
-							label: "Le super fruitier",
-							link: "/"
-						},
-						{
-							label: "Le super légumier",
-							link: "/"
-						}
-					],
-					dropdownOpen: false,
-					link: "/"
-				},
-				{
-					label: "Nos supers produits",
-					dropdown: [
-						{
-							label: "Fruits",
-							link: "/"
-						},
-						{
-							label: "Légumes",
-							link: "/"
-						},
-						{
-							label: "Épicerie",
-							link: "/"
-						}
-					],
-					dropdownOpen: false,
-					link: "/"
-				},
-				{
-					label: "Concept",
-					dropdown: null,
-					dropdownOpen: false,
-					link: "/concept"
-				},
-				{
-					label: "Nos supers recettes",
-					dropdown: null,
-					dropdownOpen: false,
-					link: "/"
-				},
-				{
-					label: "Livraison",
-					dropdown: null,
-					dropdownOpen: false,
-					link: "/livraison"
-				}
-			]
-		};
-	}
+  name: "Navbar",
+  props: {
+    isAuthenticated: Boolean,
+    updateStatus: Function
+  },
+  methods: {
+    authenticate: function() {
+      auth.login();
+      this.updateStatus();
+    },
+    logout: function() {
+      auth.logout();
+      this.updateStatus();
+    }
+  },
+  data() {
+    return {
+      items: [
+        {
+          label: "Nos supers paniers",
+          dropdown: [
+            {
+              label: "Le super mix",
+              link: "/"
+            },
+            {
+              label: "Le super fruitier",
+              link: "/"
+            },
+            {
+              label: "Le super légumier",
+              link: "/"
+            }
+          ],
+          dropdownOpen: false,
+          link: "/"
+        },
+        {
+          label: "Nos supers produits",
+          dropdown: [
+            {
+              label: "Fruits",
+              link: "/"
+            },
+            {
+              label: "Légumes",
+              link: "/"
+            },
+            {
+              label: "Épicerie",
+              link: "/"
+            }
+          ],
+          dropdownOpen: false,
+          link: "/"
+        },
+        {
+          label: "Concept",
+          dropdown: null,
+          dropdownOpen: false,
+          link: "/concept"
+        },
+        {
+          label: "Nos supers recettes",
+          dropdown: null,
+          dropdownOpen: false,
+          link: "/"
+        },
+        {
+          label: "Livraison",
+          dropdown: null,
+          dropdownOpen: false,
+          link: "/livraison"
+        }
+      ]
+    };
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
-.link
-	letter-spacing 0.1em
-	font-weight 400
-	font-size 0.9em
+.link {
+  letter-spacing: 0.1em;
+  font-weight: 400;
+  font-size: 0.9em;
+}
 
-.clickable
-	cursor pointer
+.clickable {
+  cursor: pointer;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,65 +1,65 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home/Home';
-import Authentication from './views/Authentication';
-import PageConcept from './views/PageConcept/PageConcept';
-import PageLivraison from './views/PageLivraison/PageLivraison';
-import EspaceClient from './views/EspaceClient/EspaceClient';
-import MesInformations from './views/EspaceClient/MesInformations/MesInformations';
-import MesCommandes from './views/EspaceClient/MesCommandes/MesCommandes';
-import EspaceAccueil from './views/EspaceClient/EspaceAccueil/Accueil';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home/Home";
+import Authentication from "./views/Authentication";
+import PageConcept from "./views/PageConcept/PageConcept";
+import PageLivraison from "./views/PageLivraison/PageLivraison";
+import EspaceClient from "./views/EspaceClient/EspaceClient";
+import MesInformations from "./views/EspaceClient/MesInformations/MesInformations";
+import MesCommandes from "./views/EspaceClient/MesCommandes/MesCommandes";
+import EspaceAccueil from "./views/EspaceClient/EspaceAccueil/Accueil";
 
 Vue.use(Router);
 
 export default new Router({
-	mode: 'history',
+	mode: "history",
 	base: process.env.BASE_URL,
 	routes: [
 		{
-			path: '/',
-			name: 'home',
-			component: Home,
+			path: "/",
+			name: "home",
+			component: Home
 		},
 		{
-			path: '/login',
-			name: 'login',
-			component: Authentication,
+			path: "/login",
+			name: "login",
+			component: Authentication
 		},
 		{
-			path: '/concept',
-			name: 'concept',
-			component: PageConcept,
+			path: "/concept",
+			name: "concept",
+			component: PageConcept
 		},
 		{
-			path: '/livraison',
-			name: 'livraison',
-			component: PageLivraison,
+			path: "/livraison",
+			name: "livraison",
+			component: PageLivraison
 		},
 		{
-			path: '/mon-espace',
+			path: "/mon-espace",
 			component: EspaceClient,
 			children: [
 				{
-					path: '/',
-					name: 'espaceclient',
-					component: EspaceAccueil,
+					path: "/",
+					name: "espaceclient",
+					component: EspaceAccueil
 				},
 				{
-					path: 'mes-informations',
-					name: 'mesinformations',
-					component: MesInformations,
+					path: "mes-informations",
+					name: "mesinformations",
+					component: MesInformations
 				},
 				{
-					path: 'mes-commandes',
-					name: 'mescommandes',
-					component: MesCommandes,
-				},
-			],
+					path: "mes-commandes",
+					name: "mescommandes",
+					component: MesCommandes
+				}
+			]
 		},
 		{
-			path: '*',
-			redirect: '/',
-		},
+			path: "*",
+			redirect: "/"
+		}
 		// { path: '/mon-espace*', redirect: '/mon-espace/accueil' },
 		// {
 		//   path: '/about',
@@ -69,5 +69,5 @@ export default new Router({
 		//   // which is lazy-loaded when the route is visited.
 		//   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
 		// }
-	],
+	]
 });
