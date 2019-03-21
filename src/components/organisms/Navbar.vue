9c604a64c38917b1025cbaf5dd35c83e4f159a3a
Benjamin WATERLOT <waterlotbenjamin@gmail.com>
2019-03-21 20:02:58 +0100

    minor changes

--- src/components/organisms/Navbar.vue
+++ src/components/organisms/Navbar.vue
@@ -9,19 +9,19 @@ v-toolbar.white.elevation-2(app prominent style="z-index: 1000;")
 			v-list(v-if="item.dropdown")
 				v-list-tile(@click="" v-for="dropdown in item.dropdown" :key="dropdown.label")
 					v-list-tile-title {{ dropdown.label }}
-	v-btn(flat color="primary" @click="")
+	v-btn.mr-3(flat color="primary" @click="")
 		v-badge(color="secondary")
 			v-icon(left) shopping_basket
-			span.ly.font-weight-bold 14,50 €
+			span.ly.font-weight-bold 14,50€
 			template(v-slot:badge)
-				span.white--text.caption.font-weight-bold 3
-	//- v-divider(vertical)
+				span.white--text.caption.font-weight-black 3
+	v-divider(vertical)
 	v-btn(v-if="!isAuthenticated" flat color="darkaccent" @click="authenticate()")
 		v-icon(left) account_circle
-		span Mon espace
+		span.ly Mon espace
 	v-btn.hidden-md-and-down.ml-4(v-if="isAuthenticated" flat color="darkaccent" :to="{name: 'espaceclient'}")
 		v-icon(left) face
-		span Mon espace
+		span.ly Mon espace
 	v-btn.hidden-lg-and-up.ml-4(v-if="isAuthenticated" fab small flat color="darkaccent" :to="{name: 'espaceclient'}")
 		v-icon(medium) face
 </template>
